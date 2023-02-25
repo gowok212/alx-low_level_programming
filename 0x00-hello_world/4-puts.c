92b552d91f8a0e69b4f53c4a058fae3887e95a02
Wushak Gowok <wushakfred@gmail.com>
2023-02-24 19:57:26 -0800

    Add all files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - A program that print a line with put function
- * Return: 0 															
- */
+* main - A program that print a line with put function
+* Return: 0 															
+*/
 int main(void)
 {
 put("\"Programming is like building a multilingual puzzle");
